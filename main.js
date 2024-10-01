d0aebd1e5c1b5b43178cf5167e5bb1b23bcfd9cf
marielgaribay22 <156953249+marielgaribay22@users.noreply.github.com>
2024-09-30 22:18:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Alert when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Welcome to La Dolce Vita!');
+});
+
+// click  on the logo
+document.querySelector('.logo').addEventListener('click', function() {
+    alert('Hi There!');
+});// JavaScript Document
